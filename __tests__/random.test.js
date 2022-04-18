eaa824747337c0a718d33028526b77f9dde23f54
Matthew Parker <mattparker124@gmail.com>
2022-04-18 14:35:28 -0500

    added potion code and testing

--- __tests__/random.test.js
+++ __tests__/random.test.js
@@ -1,5 +1,9 @@
-const checkIfEqual = require('../lib/random.js');
+const Potion = require('../lib/Potion.js');
 
-test('checks if 10 is equal to 10', () => {
-    expect(checkIfEqual(10, 10)).toBe(true);
+test('creates a random potion object', () => {
+    const potion = new Potion();
+
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
+    expect(potion.value).toEqual(expect.any(Number));
 });
